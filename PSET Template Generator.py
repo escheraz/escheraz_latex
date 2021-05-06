8a33b2ae019137fb021e256ef8c39306a8741920
escheraz <e6ipxcosu@gmail.com>
2021-05-06 17:28:10 -0400

    Add files via upload

---
+++ PSET Template Generator.py
@@ -0,0 +1,77 @@
+import os.path
+from os import path
+output = """\\documentclass{article}
+\\usepackage{enumitem}
+\\usepackage{amsmath}
+\\usepackage{algorithm}
+\\usepackage{algorithmic}
+\\usepackage{amssymb}
+\\usepackage{tcolorbox}
+\\usepackage[edges]{forest}
+\\usepackage{fancyhdr}
+\\usepackage[margin=1in, top=1.35in]{geometry}
+\\usepackage{tikz}
+\\setlength{\\parindent}{0pt}
+\\setlength{\\parskip}{7pt plus 1pt minus 1pt}
+
+\\newcounter{problemnum}
+\\setcounter{problemnum}{0}
+\\pagestyle{fancy}
+\\renewcommand{\\headrulewidth}{1.5pt}
+"""
+
+_class = input("Enter Class: ")
+psetnum = input("Enter PSET number: ")
+filename = _class + " PSET " + psetnum + ".tex"
+
+if(path.exists(filename)):
+	s = input("File " + filename + " already exists! Overwrite? (y/n): ")
+	if s != "y":
+		exit()
+s = input("Include Collaborators? (y/n): ")
+t = input("Number Problems Alphabetically? (y/n): ")
+
+if s == "y":
+	if t == "y":
+		output += "\\newcommand{\\problem}[1]{\\stepcounter{problemnum}  \\begin{center} \\Large \\textbf{Problem \\Alph{problemnum}} \\normalsize \\\\\\vspace{0.2cm} \\textit{Collaborators: #1}\\\\\\vspace{0.2cm}\\end{center}}"
+	else:
+		output += "\\newcommand{\\problem}[1]{\\stepcounter{problemnum}  \\begin{center} \\Large \\textbf{Problem \\theproblemnum} \\normalsize \\\\\\vspace{0.2cm} \\textit{Collaborators: #1}\\\\\\vspace{0.2cm}\\end{center}}"
+else:
+	if t == "y":
+		output += "\\newcommand{\\problem}{\\stepcounter{problemnum}  \\begin{center} \\Large \\textbf{Problem \\Alph{problemnum}} \\normalsize \\\\\\vspace{0.2cm}\\end{center}}"
+	else:
+		output += "\\newcommand{\\problem}{\\stepcounter{problemnum}  \\begin{center} \\Large \\textbf{Problem \\theproblemnum} \\normalsize \\\\\\vspace{0.2cm}\\end{center}}"
+
+output += """
+\\newcommand\\name{Name}
+\\newcommand\\email{Email}
+""" + "\\newcommand\\class{" + _class + """}
+\\newcommand\\psetnum{""" + psetnum + """}
+\\lhead{\\name}
+\\chead{\\email}
+\\rhead{\\class\\, Problem Set \\psetnum}
+
+\\begin{document}"""
+
+numProbs = int(input("Enter number of problems: "))
+
+for i in range(numProbs):
+	if s == "y":
+		output += "\n\\problem{none}\n"
+	else:
+		output += "\n\\problem\n"
+	n = int(input("Number of subparts for problem " + str(i + 1) + ": "))
+	if n != 1:
+		if t == "y":
+			output += "\\begin{enumerate}\n"
+		else:
+			output += "\\begin{enumerate}[label=(\\alph*)]\n"
+		for i in range(n):
+			output += "\t\\item \n"
+		output += "\\end{enumerate}\n"		
+	output += "\\newpage\n"
+output += "\n\\end{document}"
+f = open(f"{_class}/{filename}", "w")
+f.write(output)
+f.close()
+print("File " + filename + " successfully written.")
